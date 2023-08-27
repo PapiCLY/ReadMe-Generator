b810e568fde8c51c04abe45be10375a3a93c5e1a
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-08-26 20:50:33 -0400

    added array of questions

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'Title',
+        message: 'What is your Readme title?',
+      },
+      {
+        type: 'input',
+        name: 'Decription',
+        message: 'Enter your Readme description',
+      },
+      {
+        type: 'input',
+        name: 'License',
+        message: 'What license would you like to use?',
+      },
+      {
+        type: 'input',
+        name: 'Github',
+        message: 'Please enter your Github username',
+      },
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'Please enter your email address',
+      }   
+    ])];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
